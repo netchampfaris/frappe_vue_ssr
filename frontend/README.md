ca1cf79bff856425bd7d6090c6e15d7a3a558c54
Faris Ansari <netchamp.faris@gmail.com>
2025-06-30 15:50:15 +0530

    fix: remove frontend folder

--- frontend/README.md
+++
@@ -1,5 +0,0 @@
-# Frappe Vue SSR
-
-This is an example app to make Vue SSR work with Frappe Backend
-
-Currently work in progress.
--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-import { createApp } from './app.js'
-import router from './router'
-
-let app = createApp()
-
-app.use(router)
-app.mount('#app')
